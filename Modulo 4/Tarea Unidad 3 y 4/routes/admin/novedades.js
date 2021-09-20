38cef0e927da05d12af83803fad13ab6bb379b84
agustinsignori <signoais@gmail.com>
2021-09-19 23:53:53 -0300

    tareas modulo 5

--- Modulo 4/Tarea Unidad 3 y 4/routes/admin/novedades.js
+++ Modulo 4/Tarea Unidad 3 y 4/routes/admin/novedades.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 router.get('/', function (req, res, next) {
     res.render('admin/novedades', {
-        layout:'admin/layout'
-        //usuario: req.session.nombre
+        layout:'admin/layout',
+        usuario: req.session.nombre
     });  
 });
 
---
+++ Modulo 5/Tarea Unidad 3/routes/index.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer')
+var novedadesModel = require('../models/novedadesModel');
+var cloudinary = require('cloudinary').v2;
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades();
+
+  novedades = novedades.splice(0, 5);
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.url(novedad.img_id, {
+        width: 460,
+        crop: 'fill'
+      });
+      return {
+        ...novedad,
+        imagen
+      }
+    } else {
+      return{
+        ...novedad,
+        imagen: '/images/novedades01.jpg'
+      }
+    }
+  });
+  
+  res.render('index', {
+    novedades
+  });
+});
+
+router.post('/', async(req, res, next) => {
+
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var email = req.body.email;
+  var telefono = req.body.telefono;
+  var mensaje = req.body.mensaje;
+
+  var obj =  {
+    to: 'signoais@gmail.com',
+    subject: 'Contacto desde la Web',
+    html: nombre + " " + apellido + " se contacto a travez y quiere mas info a este correo: "
+    + email + ". <br> Ademas, hizo su primer comentario: " + mensaje + ". <br> Su telefono es " 
+    + telefono
+  }
+
+  var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  });
+
+  var info = await transport.sendMail(obj)
+
+  res.render('index', {
+    message: 'Mensaje enviado correctamente'
+  });
+});
+
+module.exports = router;
